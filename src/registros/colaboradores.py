c906245b633d6144672a1a1fc743417f01a1b62a
LuisoGITHUB <164100038+LuisoGITHUB@users.noreply.github.com>
2024-07-12 15:58:11 -0600

    limite de solo 7 colaboradores

--- src/registros/colaboradores.py
+++ src/registros/colaboradores.py
@@ -1,6 +1,11 @@
 colaboradores = {}
+max_colaboradores = 7
 
 def registrar_colaborador(codigo, nombre, clave, posicion):
+    if len(colaboradores) >= max_colaboradores:
+        print(f"No se pueden registrar mas colaboradores, el maximo son 7")
+    
+    
     if codigo in colaboradores:
         print(f"El código {codigo} ya está registrado.")
         return
@@ -8,7 +13,7 @@ def registrar_colaborador(codigo, nombre, clave, posicion):
         'nombre': nombre,
         'clave': clave,
         'posicion': posicion,
-        'horas': [0] * 5  # Asumiendo que son 6 días a la semana (lunes a sábado)
+        'horas': [0] * 6  # Asumiendo que son 6 días a la semana (lunes a sábado)
     }
     print(f"Colaborador {nombre} registrado con éxito.")
 
