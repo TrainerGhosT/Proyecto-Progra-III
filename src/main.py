9eeabada336a983c8b06f5242c9b8214038c150f
TrainerGhosT <dylanmont15@gmail.com>
2024-07-11 23:32:59 -0600

    Archivo main más limpio

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-# main.py
 
-from menu.menu import menu_mostrar
+from menu.menu import menu
 
 def main():
-    menu_mostrar()
+    menu()
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
