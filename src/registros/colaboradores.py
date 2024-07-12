edd238552c6e5b47b02761c16ff83a2b93614bc1
LuisoGITHUB <164100038+LuisoGITHUB@users.noreply.github.com>
2024-07-12 14:26:15 -0600

    Ignorar
    Solo estaba probando algo

--- src/registros/colaboradores.py
+++ src/registros/colaboradores.py
@@ -8,7 +8,7 @@ def registrar_colaborador(codigo, nombre, clave, posicion):
         'nombre': nombre,
         'clave': clave,
         'posicion': posicion,
-        'horas': [0] * 6  # Asumiendo que son 6 días a la semana (lunes a sábado)
+        'horas': [0] * 5  # Asumiendo que son 6 días a la semana (lunes a sábado)
     }
     print(f"Colaborador {nombre} registrado con éxito.")
 
